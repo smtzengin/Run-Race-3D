7500d49bed0d83f815ecbb6f2b2b2cafb66e20ca
Samet Zengin <73519045+smtzengin@users.noreply.github.com>
2023-03-05 16:12:59 +0300

    Animations
    Animations added to player. It's work.

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour
     private Vector3 move;
     public float speed, jumpForce, gravity, verticalVelocity;
     private CharacterController charController;
-    private bool doubleJump;
+    private bool wallSlide;
+    private Animator anim;
 
     private void Awake()
     {
         charController = GetComponent<CharacterController>();
+        anim = transform.GetChild(0).GetComponent<Animator>();
     }
 
     void Update()
@@ -20,44 +22,55 @@ public class PlayerController : MonoBehaviour
 
         if (charController.isGrounded)
         {
+            wallSlide = false;
             verticalVelocity = 0;
             if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
             {
-                verticalVelocity = jumpForce;
-                doubleJump = true;
+                Jump();
             }           
         }
-        else
+        if (!wallSlide)
         {
             gravity = 30;
             verticalVelocity -= gravity * Time.deltaTime;
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) && doubleJump)
-            {
-                verticalVelocity += jumpForce * .5f;
-                doubleJump = false;
-            }
+            anim.SetBool("WallSlide", true);
         }
+        else
+        {
+            gravity = 15;
+            verticalVelocity -= gravity * Time.deltaTime;
+            anim.SetBool("WallSlide", false);
+        }
+
+        anim.SetBool("WallSlide", wallSlide);
+        anim.SetBool("Grounded", charController.isGrounded);
+
         move.Normalize();
         move *= speed;
 
         move.y = verticalVelocity;
         charController.Move(move * Time.deltaTime);
+        
     }
 
+    void Jump()
+    {
+        verticalVelocity = jumpForce;
+        anim.SetTrigger("Jump");
+    }
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (!charController.isGrounded)
         {
             if(hit.collider.tag == "Wall")
             {
-
+                if (verticalVelocity < -.7f)
+                    wallSlide = true;
                 if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
                 {
-                    verticalVelocity = jumpForce;
-
-                    doubleJump = false;
-
+                    Jump();
                     transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 180, transform.eulerAngles.z);
+                    wallSlide = false;
                 }
             }
         }
